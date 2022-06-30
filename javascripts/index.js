e7b9699907fedbb658bb2ef4931b100dc07db9e7
Maciej Matula <maciek.matula@gmail.com>
2022-06-29 20:13:24 -0500

    created function to add ingredients to ingredient list

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-
+  addIngredient()
 })
 
+//function that adds ingredients to ingredient list
 function addIngredient(){
-  const 
+  const ingredientBtn = document.querySelector('#ingredient_add')
+  const ingredientField = document.querySelector('#recipe_ingredients')
+  const ul = document.querySelector('#list_of_ingredients')
+
+    ingredientBtn.addEventListener('click', (event)=>{
+      event.preventDefault()
+      const li = document.createElement('li')
+      li.innerText = ingredientField.value
+      ul.appendChild(li)
+      ingredientField.value = ''
+    })
 }
